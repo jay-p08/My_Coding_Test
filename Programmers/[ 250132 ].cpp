641708b87b798b2c95201e3bf32a5387d2eaf648
parkjay0709 <soRjdla0709@gmail.com>
2025-08-21 09:53:18 +0900

    [ Programmers ] [ PCCE 기출문제 ] 2번 피타고라스의 정리 / 250132 / Level.0
    https://school.programmers.co.kr/learn/courses/30/lessons/250132

--- Programmers/[ 250132 ].cpp
+++ Programmers/[ 250132 ].cpp
@@ -2,12 +2,13 @@
 
 using namespace std;
 
-int main(void) {
+int main(void)
+{
     int a;
     int c;
     cin >> a >> c;
-    
-    int b_square = c*c - a*a;
+
+    int b_square = c * c - a * a;
     cout << b_square << endl;
     return 0;
 }
